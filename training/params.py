5a8a72ed12928d2951570432ae38e5938fd33833
Ola Hamdy <34010477+OlaHamdy3@users.noreply.github.com>
2020-04-25 14:35:22 +0200

    updated config to train darknet on kitti

--- training/params.py
+++ training/params.py
@@ -1,14 +1,14 @@
 TRAINING_PARAMS = \
 {
     "model_params": {
-        "backbone_name": "shufflenet_2",
-        "backbone_pretrained": "../weights/shufflenetv2.pth", #  set empty to disable
+        "backbone_name": "darknet_53",
+        "backbone_pretrained": "../weights/darknet53_weights.pth", #  set empty to disable
     },
     "yolo": {
         "anchors": [[[116, 90], [156, 198], [373, 326]],
                     [[30, 61], [62, 45], [59, 119]],
                     [[10, 13], [16, 30], [33, 23]]],
-        "classes": 1,
+        "classes": 8,
     },
     "lr": {
         "backbone_lr": 0.001,
@@ -22,12 +22,12 @@ TRAINING_PARAMS = \
         "weight_decay": 4e-05,
     },
     "batch_size": 8,
-    "train_path": "/content/ShuffleNetv2-YOLOv3/data/cones_paths.txt",
+    "train_path": "/content/ShuffleNetv2-YOLOv3/data/Kitti_paths.txt",
     "mix": False,
     "no_mixup_epochs": 0,
     "epochs": 100,
-    "img_h": 414,
-    "img_w": 414,
+    "img_h": 4,
+    "img_w": 1200,
     "parallels": [0,1,2,3],                         #  config GPU device
     "working_dir": "YOUR_WORKING_DIR",              #  replace with your working dir
     "pretrain_snapshot": "",                        #  load checkpoint
